8fc71b185f4fd366aade1feb788c1b87539a0f98
James Jordan <jpjordan1987@gmail.com>
2021-02-16 23:58:48 -0500

    finished!

--- script.js
+++ script.js
@@ -13,75 +13,49 @@ var lowercaseArray;
 var numbersArray;
 var symbolArray;
 
-var multiSelector;
-var actionInstruction;
-var chgHeading;
 var generateBtn;
 var validInput;
 var index;
+var fillExtra;
+
+var lowerUse;
+var upperUse;
+var numberUse;
+var symbolUse;
+
+var lowercaseUseArray=[];
+var uppercaseUseArray=[];
+var numbersUseArray=[];
+var symbolUseArray=[];
 
 lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 numbersArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-symbolArray = ['"', '!', '"', '#', '$', '%', '&', '(',')',' * ',' + ',', ',' - ','.',' / ',': ','; ',' < ','=',' > ',' ? ','@','[','^','`','{','|','}','~'];
+symbolArray = ['"', '!', '"', '#', '%', '&','-','/',':',';','<','=','>','?','@','`','{','}','~'];
 
-//Immediate Changes To View on Page Load - hide items
-displayChangeSecuritySettings();
+
+
+//displayChangeSecuritySettings();
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var proceedBtn = document.querySelector("#proceed");
 
 // Add event listener to generate button
 document.addEventListener("DOMContentLoaded", generateBtn.addEventListener("click", generatePassword));
-document.addEventListener("DOMContentLoaded", proceedBtn.addEventListener("click", calculatePwd));
+//document.addEventListener("DOMContentLoaded", proceedBtn.addEventListener("click", calculatePwd));
 
-// Generate a password    --- This is just the initial activation step in making a password                                           
+// Generate a password                                           
 function generatePassword() {
     chgHeading = true;
-    displayChangeSecuritySettings(); //Prompt Step Complete
+   //Prompt Step Below
     secLength = window.prompt("How many characters would you like your password to be (# between 8 and 128)?");
-    validatePwdInput(); //Validation Step Complete
+    validatePwdInput(); //Validation Step Below
     if (validInput === false) {
         alert("Your input was invalid. Please start over.");
         location.reload();
         return;
     }
-}
-
-// View/Hide Multi Select Menu with appropriate header                                            
-function displayChangeSecuritySettings() {
-    multiSelector = document.querySelector("#selectordisplay");
-    passwordDisplay = document.querySelector("#pwddisplay");
-    actionInstruction = document.querySelector("h2");
-    proceedBtn = document.querySelector("#proceed")
-    generateBtn = document.querySelector("#generate");
-
-    if (chgHeading) {
-        //Change Action Instruction
-        if (actionInstruction.innerHTML !== "Generate a Password") {
-            actionInstruction.innerHTML = "Generate a Password";
-        } else {
-            actionInstruction.innerHTML = "Choose Security Settings";
-        }
-    }
-            //Change Whether Text Area or Settings Selector Shows
-        if (multiSelector.style.display === "none") {
-            multiSelector.style.display = "block";
-            passwordDisplay.style.display = "none";
-        } else {
-            multiSelector.style.display = "none";
-            passwordDisplay.style.display = "block";
-        }
-            //Change Whether Proceed or Generate Button Shows
-        if (proceedBtn.style.display === "none") {
-            proceedBtn.style.display = "block";
-            generateBtn.style.display = "none"
-        } else {
-            proceedBtn.style.display = "none";
-            generateBtn.style.display = "block";
-        }
-
+    calculatePwd()
 }
 
 //Validate Password Length
@@ -108,49 +82,99 @@ function validatePwdInput() {
     }
 }
 
-//Validate Password Length
-
 function calculatePwd() {
-index=1;
-var i=0;
-var upperItem;
-var numberItem;
-var symbolItem;
-var selected = [];
-multiSelector=document.querySelector("#selectordisplay");
-    //Populate List and Count of Types of Characters Chosen
-for (var option of document.getElementById("selectordisplay").options) {
-    if (option.selected) {
-      selected.push(option.value);
-      i=i+1;
-    }
-  }
-    //Add 1 from uppercase array if uppercase chosen
-    if (selected.includes("Uppercase")) { 
-    upperItem = uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)].toString();
-    } else { upperItem = "";
-}
-    //Add 1 from numbers array if numbers chosen
-    if (selected.includes("Numbers")) { 
-        numberItem = numbersArray[Math.floor(Math.random() * numbersArray.length)].toString();
-    } else { numberItem = "";
-}
-    //Add 1 from symbols array if symbols chosen
-    if (selected.includes("Symbols")) { 
-        symbolItem = symbolArray[Math.floor(Math.random() * symbolArray.length)].toString();
-    } else { symbolItem = "";
-}
-    //password before lowercase fill
-    password=upperItem.concat(numberItem,symbolItem);
 
-    //lowercase fill to chosen length
-    for (var j = 0; j < (secLength-i); j++) { 
-        password=password.concat(lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)].toString());
+    index=1;
+    var i=0;
+    var j=0;
+    lowerUse=confirm("Do you want lowercase letters in your password?");
+    upperUse=confirm("Do you want UPPERCASE letters in your password?");
+    numberUse=confirm("Do you want numbers in your password?");
+    symbolUse=confirm("Do you want symbols in your password?");
+    var selected = [];
+    var extraFilledAlready=false;
+
+    if (lowerUse){
+        i++;
     }
+    if (upperUse){
+        i++;
+    }
+    if (numberUse){
+        i++;
+    }
+    if (symbolUse){
+        i++;
+    }
+
+    //Determine number of remaining to fill
+    if ((secLength%i) !== 0 ) {
+        fillExtra=(secLength%i);
+    } else {
+        extraFilledAlready=true;
+    }
+
+    //Populate List and Count of Types of Characters Chosen
+    //Add 1/i from array if array chosen, fillExtra if any remain
+
+    if (upperUse) {
+        for (var j = 0; j < Math.floor((secLength/i)); j++) { 
+            uppercaseUseArray.push(uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)]);
+        } if (!extraFilledAlready){for (var j = 0; j < fillExtra; j++) { 
+            uppercaseUseArray.push(uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)]);
+            extraFilledAlready=true;
+        }
+    }
+}
+    //Add 1/i from array if array chosen, fillExtra if any remain
+    if (lowerUse) {
+        for (var j = 0; j < Math.floor((secLength/i)); j++) { 
+            lowercaseUseArray.push(lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)]);
+        } if (!extraFilledAlready){for (var j = 0; j < fillExtra; j++) { 
+            lowercaseUseArray.push(lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)]);
+            extraFilledAlready=true;
+        }
+    }
+}
+    //Add 1/i from array if array chosen, fillExtra if any remain
+
+    if (numberUse) {
+        for (var j = 0; j < Math.floor((secLength/i)); j++) { 
+            numbersUseArray.push(numbersArray[Math.floor(Math.random() * numbersArray.length)]);
+        } if (!extraFilledAlready){for (var j = 0; j < fillExtra; j++) { 
+            numbersUseArray.push(numbersArray[Math.floor(Math.random() * numbersArray.length)]);
+            extraFilledAlready=true;
+        }
+    }
+}
+    //Add 1/i from array if array chosen, fillExtra if any remain
+
+    if (symbolUse) {
+        for (var j = 0; j < Math.floor((secLength/i)); j++) { 
+            symbolUseArray.push(symbolArray[Math.floor(Math.random() * symbolArray.length)]);
+        } if (!extraFilledAlready){for (var j = 0; j < fillExtra; j++) { 
+            symbolUseArray.push(symbolArray[Math.floor(Math.random() * symbolArray.length)]);
+            extraFilledAlready=true;
+        }
+    }
+}
+
+    password=((uppercaseUseArray.concat(numbersUseArray,symbolUseArray,lowercaseUseArray)).toString());
+
+//Replace all function thanks to https://dmitripavlutin.com/replace-all-string-occurrences-javascript/
+const search = ',';
+const replaceWith = '';
+
+    const mypassword=password.replaceAll(search,replaceWith)
+
+    const search2 = ' ';
+    const replaceWith2 = '';
+
+    const mypassword2=mypassword.replaceAll(search2,replaceWith2)
 
     //randomize order - thanks to stack overflow question 3943772
-    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-    displayChangeSecuritySettings();
+    password=mypassword2.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+    //print to page
     document.querySelector("#password").innerHTML = password;
 }
-
